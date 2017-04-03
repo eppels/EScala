97f0f866a16276b7cb2e240c9e393b89c6331937
Eppels <Eppels@gmail.com>
2017-04-03 00:50:01 -0400

    saving initial working program and a gitignore so my intellij stuff doesn't get committed

---
+++ src/Calculator.scala
@@ -0,0 +1,50 @@
+/**
+ * Extended Exercise 1 - A calculator
+ * Chose to implement eval using pattern matching because I thought it would be easier to see the implementtation all in one place instead of spread across the classes
+ * Didn't use a separate object to implement because only using the data in the classes to implement the method
+ */
+
+sealed trait Expression {
+  def eval : EvalResult = this match {
+    case Number(value) => Success(value)
+    case Addition(left, right) => (left.eval, right.eval) match {
+      case (Failure(msg), _) => Failure(msg)
+      case (_, Failure(msg)) => Failure(msg)
+      case _ => Success(left.eval.result + right.eval.result)
+    }
+    case Subtraction(left, right) => (left.eval, right.eval) match {
+      case (Failure(msg), _) => Failure(msg)
+      case (_, Failure(msg)) => Failure(msg)
+      case _ => Success(left.eval.result - right.eval.result)
+    }
+    case Division(numerator, denominator) => if (denominator.eval.result == 0) Failure("Division by zero") else (numerator.eval, denominator.eval) match {
+      case (Failure(msg), _) => Failure(msg)
+      case (_, Failure(msg)) => Failure(msg)
+      case _ => Success(numerator.eval.result / denominator.eval.result)
+    }
+    case SquareRoot(value) => if (value.eval.result < 0) Failure("Square root of negative number") else value.eval match {
+      case Failure(msg) => Failure(msg)
+      case _ => Success(Math.sqrt(value.eval.result))
+    }
+  }
+}
+final case class Addition(left: Expression, right: Expression) extends Expression
+final case class Subtraction(left: Expression, right: Expression) extends Expression
+final case class Division(numerator: Expression, denominator: Expression) extends Expression
+final case class SquareRoot(value: Expression) extends Expression
+final case class Number(value: Double) extends Expression
+
+sealed trait EvalResult {
+  def result : Double
+}
+final case class Success(result: Double) extends EvalResult
+final case class Failure(reason: String) extends EvalResult {
+  val result = Double.NaN
+}
+
+object TestCalculator extends App {
+  println(SquareRoot(Number(-1.0)).eval)
+  println(Addition(SquareRoot(Number(-1.0)), Number(2.0)).eval)
+  assert(Addition(SquareRoot(Number(4.0)), Number(2.0)).eval == Success(4.0))
+  assert(Division(Number(4), Number(0)).eval == Failure("Division by zero"))
+}
---
+++ src/Music.scala
@@ -0,0 +1,25 @@
+/**
+ * Extended Exercise 3 - Music
+ * Not entirely sure what to do with this one - choosing to model Songs, Artists, Albums, Playlists, Genres in a pretty simple way that could be extensible
+ * Limitations/Questions:
+ * 1. Is my expression of genre good or is there an Enum or something that I should use instead?
+ * 2. More fields for Song or Artist? Any fields for genre?
+ * 3. Tried to make expandable to media beyond music (aka movie)
+ */
+sealed trait Genre
+case object Pop extends Genre
+case object Rock extends Genre
+case object Jazz extends Genre
+//more here...
+
+sealed trait Artist
+final case class SoloArtist(name: String) extends Artist
+final case class Band(bandName: String, members: Seq[Artist]) extends Artist //wonder if this should be restricted to solo artist...
+final case class Collaboration(members: Seq[Artist]) extends Artist
+
+sealed trait Multimedia
+final case class Song(name: String, artist: Artist, genre: Set[Genre], length: Double) extends Multimedia //genre a set because a song could fall into more than one
+//could maybe model movies or other types of library items too, but limitng this to music
+
+sealed trait MultimediaCollection
+final case class PlayList(members: Seq[Multimedia]) extends MultimediaCollection
